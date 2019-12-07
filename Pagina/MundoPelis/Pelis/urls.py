14912cee1ebcb082f70e17f14f6b7d33f8394a09
Enzo Lizana <57926126+lizanaenzo@users.noreply.github.com>
2019-12-06 23:48:53 -0300

    Add files via upload

--- Pagina/MundoPelis/Pelis/urls.py
+++ Pagina/MundoPelis/Pelis/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.home, name = 'inicio'),
     url('crear_persona', views.CreatePersona.as_view(), name = "crear_persona"),
     url(r'^listar_persona/',views.ListPersona.as_view(), name="listar_persona"),
-    url(r'^editar_persona/',views.UpdatePersona.as_view(), name="editar_persona"),
-    url(r'^eliminar_persona/',views.DeletePersona.as_view() , name="eliminar_persona"),
+    url(r'^editar_persona/(?P<id>\d+)/$',views.UpdatePersona.as_view(), name="editar_persona"),
+    url(r'^eliminar_persona/(?P<id>\d+)/$',views.DeletePersona.as_view() , name="eliminar_persona"),
     ]
